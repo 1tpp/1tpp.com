eac4cd711b9dd3d7b05bcb902e6c686d95b5a15b
1tpp <contact@1tpp.com>
2021-11-21 18:08:44 +0700

    feat: add three/fiber dependency

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,41 @@
 import type { NextPage } from 'next'
+import { useRef, useState } from 'react'
+
+import * as THREE from 'three'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { BoxGeometry } from 'three'
+
+function Box(props: JSX.IntrinsicElements['mesh']) {
+  const mesh = useRef<THREE.Mesh>(null!)
+  const [hovered, setHovered] = useState(false)
+  const [active, setActive] = useState(false)
+
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHovered(true)}
+      onPointerOut={(event) => setHovered(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto">
-      <div className="flex h-screen">
-        <div className="w-1/2">
-          <h1 className="text-2xl font-bold">
-            <span className="text-blue-500">Hello</span>
-            <span className="text-red-500">World</span>
-          </h1>
-          <p className="text-gray-500">
-            This is a simple example of a Next.js page.
-          </p>
-        </div>
-      </div>
+    <div className="container mx-auto h-screen">
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
     </div>
   )
 }
