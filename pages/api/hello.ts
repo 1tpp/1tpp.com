6776e69819f21899fe002bd5b96e3fe506e778c3
1tpp <contact@1tpp.com>
2022-01-04 23:24:18 +0700

    feat: api/hello
    who am i

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
+  aka: string
+  age: number
+  school: string
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    name: 'Panapat Pilapa',
+    aka: '1tpp',
+    age: 17,
+    school: 'Ubon Ratchathani Technical College',
+  })
 }
